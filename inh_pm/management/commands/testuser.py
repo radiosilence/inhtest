4069eb0f9793fcd935870837141f4f412c1fbec5
James Cleveland <jc@blit.cc>
2014-07-31 12:27:16 +0100

    fix user

--- inh_pm/management/commands/testuser.py
+++ inh_pm/management/commands/testuser.py
@@ -5,9 +5,12 @@ class Command(BaseCommand):
     help = "Backup the scaffold data from here to the remote backup server"
 
     def handle(self, *args, **options):
-        User.objects.create(
+        u = User.objects.create(
             username='test',
             email='test@example.com',
-            password=u'pbkdf2_sha256$12000$rLnMaukpmTqb$vjOIjdSkG6VBApWzPVJO/1IXBWIkCs9duNJpVfQ5cDc=',  # hi
+            password=u'test',  # test
             is_superuser=True,
+            is_staff=True
         )
+        u.set_password('test')
+        u.save()
